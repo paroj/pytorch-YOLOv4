762da0e4e9e453276ea7370e21d72f02bce4f03c
Pavel Rojtberg <rojtberg@gmail.com>
2022-04-27 14:55:01 +0200

    camera: use pytorch native model

--- camera.py
+++ camera.py
@@ -10,12 +10,15 @@
     @Detail    :
 
 '''
-from __future__ import division
 import cv2
-from tool.darknet2pytorch import Darknet
 import argparse
-from tool.utils import *
-from tool.torch_utils import *
+import time
+
+import torch
+from tool.utils import load_class_names, plot_boxes_cv2
+from tool.torch_utils import do_detect
+
+from models import Yolov4
 
 
 def arg_parse():
@@ -34,25 +37,26 @@ def arg_parse():
 
 
 if __name__ == '__main__':
-    cfgfile = "cfg/yolov4.cfg"
-    weightsfile = "weight/yolov4.weights"
+    weightsfile = "yolov4.pth"
 
     args = arg_parse()
     confidence = float(args.confidence)
     nms_thesh = float(args.nms_thresh)
     CUDA = torch.cuda.is_available()
     num_classes = 80
-    bbox_attrs = 5 + num_classes
     class_names = load_class_names("data/coco.names")
 
-    model = Darknet(cfgfile)
-    model.load_weights(weightsfile)
+    model = Yolov4(n_classes=num_classes, inference=True)
+
+    pretrained_dict = torch.load(weightsfile, map_location=torch.device('cuda'))
+    model.load_state_dict(pretrained_dict)
 
     if CUDA:
         model.cuda()
 
     model.eval()
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
     assert cap.isOpened(), 'Cannot capture source'
 
@@ -61,15 +65,15 @@ if __name__ == '__main__':
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            sized = cv2.resize(frame, (model.width, model.height))
+            sized = cv2.resize(frame, (416, 416))
             sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-            boxes = do_detect(model, sized, 0.5, 0.4, CUDA)
+            boxes = do_detect(model, sized, 0.4, 0.6, CUDA)
 
-            orig_im = plot_boxes_cv2(frame, boxes, class_names=class_names)
+            orig_im = plot_boxes_cv2(frame, boxes[0], class_names=class_names)
 
             cv2.imshow("frame", orig_im)
             key = cv2.waitKey(1)
-            if key & 0xFF == ord('q'):
+            if key == 27:
                 break
             frames += 1
             print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
